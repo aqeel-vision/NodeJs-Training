1eae9801c5933ddc6182bd5cfa8439af60c87ada
aqeel-vision <aqeel261@hotmail.com>
2023-06-21 03:00:58 +0500

    NodeJS Assignment 2

--- app.js
+++ app.js
@@ -1,33 +1,156 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-const hostname = 'localhost';
-const port = 3000;
+const app = express();
+app.use(express.json());
 
-const server = http.createServer((request, response) => {
+// Connect to MongoDB Atlas
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB Atlas');
+});
 
-    response.writeHead(200, {'Content-Type': 'text/html'});
+// User schema
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
 
-    const url = request.url;
+const User = mongoose.model('User', userSchema);
 
-    if(url ==='/') {
-        response.write('This is default page, <a href="/read-file">Click here to see file content </a>');
-        response.end();
-    }
-    else if(url ==='/read-file') {
-        fs.readFile('whatIsNodeJs.txt', function(err, data) {
-            response.write(data);
-            return response.end();
-        });
-    }
-    else {
-        response.write('Request not handled');
-        response.end();
-    }
+// Routes
+
+// Get all users
+app.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+
 
 });
 
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+// Create a new user
+app.post('/api/users', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
+
+// Update a user
+app.put('/api/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { password } = req.body;
+
+    // Hash the new password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Update the user
+    await User.findByIdAndUpdate(id, { password: hashedPassword });
+
+    res.json({ message: 'User updated successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Delete a user
+app.delete('/api/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Delete the user
+    await User.findByIdAndDelete(id);
+
+    res.json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Login route
+app.post('/api/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Find the user
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    // Validate the password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    // Create and return a JWT token
+    const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET);
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Middleware to protect private routes
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
+// Protected route
+app.get('/api/profile', authenticateToken, (req, res) => {
+  res.json({ message: 'This is a protected route', user: req.user });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+In this assignment we have apis for create, update, delete, view all and register user API,
+
+The profile API is protected we need to pass JWT as bearer token and the user information will be returned if JWT is valid.
+
+we have the following routes:
+
+Create a new user: POST /api/users
+View all users: GET /api/users
+
+Update a user: PUT /api/users/:id
+Delete a user: DELETE /api/users/:id
+
+Create a new user: POST /api/users
+Access a protected route (Profile): GET /api/profile
